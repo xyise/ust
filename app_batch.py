ca0893a5e08b80a8f71e3b7adcd8e9feb0098fa7
anylox <lee.youngsuk@gmail.com>
2020-08-21 20:29:55 +0100

    minor update in the app batch

--- app_batch.py
+++ app_batch.py
@@ -8,13 +8,14 @@ def run_me(final_date):
 
     dn = db_manager_UST()
 
-    for dt in date_range:
+    for idx, dt in enumerate(date_range):
         print('***** ' + str(dt) + ' *****')
         dn.update(dt)
+        if idx < len(date_range) - 7:
+            print('setting the date as confirmed')
+            dn._update_confirmed_date(dt, True)
         time.sleep(.5)
-
-
-
+        
 if __name__ == '__main__':
 
     today = datetime.datetime.today()    
